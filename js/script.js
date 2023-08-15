51f652363de96879edcb9778a486c143aa2c3474
florinasutanto <florinasutanto@gmail.com>
2023-08-14 20:37:40 -0400

    update

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ const container = d3.select("#image-container");
 // Create rows to group images
 const rows = container
   .selectAll("div.row")
-  .data(d3.range(Math.ceil(imageList.length / 5))) // Calculate the number of rows needed
+  .data(d3.range(Math.ceil(imageList.length / 5))) // Number of rows
   .enter()
   .append("div")
-  .attr("class", "row"); // Add a class to style the rows (optional)
+  .attr("class", "row"); // 
 
 // Load the images and append them to each row
 rows
@@ -29,9 +29,9 @@ rows
   .append("img")
   .attr("src", (d) => d)
   .attr("alt", (d, i) => "Image " + (i + 1))
-  .attr("id", (d, i) => "image-" + (imageList.indexOf(d) + 1)) // Add the "image-" prefix to the ids
+  .attr("id", (d, i) => "image-" + (imageList.indexOf(d) + 1)) 
   .style("height", "4.2vw") // Set the height to 4.2% of viewport
-  .style("margin-right", "10px") // Add some spacing between images
+  .style("margin-right", "10px") 
   .style("filter", "grayscale(100%)") // Apply grayscale to all images initially
   .style("opacity", "0.5") // Set opacity to 50% for all images initially
 
